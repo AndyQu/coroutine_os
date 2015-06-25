5c6458c768065a9e00e2c1f3960e26aa8a6dfd50
曲国铖 <quguocheng@meituan.com>
2015-06-25 22:03:54 +0800

    add non blocking io

--- task.py
+++ task.py
@@ -1,6 +1,9 @@
 __author__ = 'andy'
 import time
 
+def debug(log):
+    print log
+
 def coroutine(func):
     def start(*args,**kwargs):
         cr = func(*args,**kwargs)
@@ -11,11 +14,12 @@ def coroutine(func):
 class Task:
     id = 1
 
-    def __init__(self, routine):
+    def __init__(self, routine, name):
         self.coroutine = routine
         self.value = None
         self.pid = Task.id
         self.waiting_tasks=[]
+        self.task_name=name
 
         print "new task",self.pid
         Task.id += 1
@@ -67,6 +71,20 @@ class WaitTask(SystemCall):
     def handle(self):
         self.scheduler.wait(self.task, self.waited_task_id)
 
+class WaitRead(SystemCall):
+    def __init__(self,file):
+        self.file=file
+
+    def handle(self):
+        self.scheduler.wait_for_read(self.task, self.file.fileno())
+
+class WaitWrite(SystemCall):
+    def __init__(self,file):
+        self.file=file
+
+    def handle(self):
+        self.scheduler.wait_for_write(self.task, self.file.fileno())
+
 @coroutine
 def tick():
     yield
@@ -95,21 +113,51 @@ def forker():
     # yield KillTask(sub_task_id)
     # print "killed my sub task",sub_task_id
 
+import select
 class Scheduler:
     def __init__(self):
         self.ready_tasks=[]
         self.killed_tasks={}
         self.finished_tasks={}
-        self.read_wait_tasks=[]
-        self.write_wait_tasks=[]
+        self.wait_read_tasks={}
+        self.wait_write_tasks={}
         self.wait_other_tasks={}
         self.task_map={}
+        self.task_index=-1
+
+    def new_task(self, routine, name):
+        self.schedule(Task(routine,name))
+
+
+
+    def make_read_ready(self, fd):
+        task=self.wait_read_tasks.pop(fd, None)
+        self.ready_tasks.append(task)
+        print "read ready for task", task.pid
+
+    def make_write_ready(self, fd):
+        task=self.wait_write_tasks.pop(fd, None)
+        self.ready_tasks.append(task)
+        print "write ready for task", task.pid
+
+    def wait_for_read(self, task, fd):
+        self.ready_tasks.remove(task)
+        self.wait_read_tasks[fd]=task
+        print "move task from ready to io/read queue",task.task_name
+
+    def wait_for_write(self, task, fd):
+        self.ready_tasks.remove(task)
+        self.wait_write_tasks[fd]=task
+        print "move task from ready to io/write queue",task.task_name
 
     def has_tasks(self):
         return len(self.ready_tasks)>0
 
     def fetch(self):
-        return self.ready_tasks.pop(0)
+        self.task_index+=1
+        if self.task_index>=len(self.ready_tasks):
+            self.task_index=0
+        return self.ready_tasks[self.task_index]
 
     def schedule(self, task):
         self.ready_tasks.append(task)
@@ -135,12 +183,23 @@ class Scheduler:
         print "task [{}] ended".format(task.pid)
         self.finished_tasks[task.pid]=task
 
+    # @coroutine
+    def io_loop(self):
+        yield
+        while True:
+            r,w,e=select.select(self.wait_read_tasks.keys(), self.wait_write_tasks.keys(), [], 5)
+            for read_ready_fd in r:
+                self.make_read_ready(read_ready_fd)
+            for write_ready_fd in w:
+                self.make_write_ready(write_ready_fd)
+            yield
 
     def schedule_waiting_tasks(self,task):
         for waiting_task in self.wait_other_tasks.get(task.pid,[]):
             self.schedule(waiting_task)
 
     def loop(self):
+        self.new_task(self.io_loop(),"io_loop")
         while True:
             if not self.has_tasks():
                 print "no ready task, sleeping 10s......"
@@ -151,8 +210,8 @@ class Scheduler:
     def run_once(self):
         try:
             task = self.fetch()
+            debug( "to run task {}".format(task.task_name))
             request = task.resume()
-            self.schedule(task)
             if isinstance(request, SystemCall):
                 request.task = task
                 request.scheduler=self
@@ -164,9 +223,42 @@ class Scheduler:
     def has_waiting_task(self, task):
         pass
 
+@coroutine
+def handle_client(client, addr):
+    yield
+    print "Connection from", addr
+    while True:
+        yield WaitRead
+        data = client.recv(65536)
+        if not data:
+            break
+        yield WaitWrite
+        client.send(data)
+    client.close()
+    print "Client closed"
+    yield           # Make the function a generator/coroutine
+
+from socket import *
+@coroutine
+def server(port):
+    yield
+    print "Server starting"
+    sock = socket(AF_INET,SOCK_STREAM)
+    sock.bind(("",port))
+    sock.listen(5)
+    while True:
+        print "wait read"
+        yield WaitRead(sock)
+        print "wait done"
+        client,addr = sock.accept()
+        yield NewTask(handle_client(client,addr))
+
+
+
 if __name__ == "__main__":
     scheduler = Scheduler()
-    scheduler.schedule(Task(tick()))
-    scheduler.schedule(Task(tock()))
-    scheduler.schedule(Task(forker()))
+    scheduler.new_task(tick(),"tick")
+    scheduler.new_task(tock(),"tock")
+    # scheduler.schedule(Task(forker()))
+    scheduler.new_task(server(9000),"server")
     scheduler.loop()
